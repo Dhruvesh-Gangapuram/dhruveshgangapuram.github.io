a6e6b6b9c31d09d64e68d4fe4a1d739766d43fe6
Dhruvesh-Gangapuram <gangapuramdhruvesh@gmail.com>
2023-08-01 13:00:31 +0530

    updated mobile view

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
+let s_button = document.getElementById("btn_top");
+let imgs = document.querySelectorAll(".m_container img");
+
+window.addEventListener("scroll", () => {
+  s_button.style.opacity = "1";
+  s_button.style.transition = "5s";
+});
+
+s_button.addEventListener("click", () => {
+  window.scrollTo(0, 0);
+});
+
 window.addEventListener("scroll", () => {
   var my_name = document.getElementById("name");
   let value = window.scrollY;
-  my_name.style.marginTop = value + "px";
+  my_name.style.marginTop = -value + "px";
 });
